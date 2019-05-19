916385ebe82e265d92508022804a09e9f0618d08
loueasey <luise.antonia@hotmail.de>
2019-05-19 18:50:27 +0200

    blog comments feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   Blogger::Application.routes.draw do
-    resources :articles
+    root to: 'articles#index' 	
+    resources :articles do
+	  resources :comments
+	end
   end
 end
 
